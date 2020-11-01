cc4e2498d27bb5fbcebbe5864d4562fdff1ce696
ava brenneke <avabrenneke@gmail.com>
2020-10-31 17:59:29 -0700

    addes start function and timer

--- script.js
+++ script.js
@@ -91,3 +91,37 @@ const initialTime = 1; //minutes
 let started = false;
 let score = 0;
 let totalScores = [];
+
+function start() {
+    started = true;
+    score = 0
+    //reset timer
+    var timerTime = 60 * initialTime;
+    startTimer(timerTime, display)
+    //startTime();
+    updateScores();
+
+    //reset the question position
+    currentQuestionPosition = 0;
+    nextQuestion();
+}
+//Timer
+function startTimer(duration, display) {
+    timer = duration;
+    let minutes, seconds;
+
+    timerInterval = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            clearInterval(timerInterval);
+            endQuiz();
+        }
+    }, 1000);
+}
