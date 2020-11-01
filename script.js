1ac4eee8e9725f84b1d18c216e28bca0ee1e866e
ava brenneke <avabrenneke@gmail.com>
2020-10-31 17:57:00 -0700

    added initial JS defining global variables

---
+++ script.js
@@ -0,0 +1,8 @@
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
+const timerButton = document.getElementById("startTime");
+const display = document.querySelector('#timer');
+
+let Allscores = [];
+let timer = null;
+let timerInterval = null;
